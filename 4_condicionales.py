67a3ed22b2d83d85d68313f53e740dc92ce988bd
Giovani Rodriguez <travelli@live.com.mx>
2020-04-28 19:07:44 -0500

    Ejercicios

---
+++ 4_condicionales.py
@@ -0,0 +1,14 @@
+# Manejo basico de condicionales
+# %%
+# Define un saludo diferente basado en la variable 'sexo'
+
+# Usa alguna de las siquientes opciones
+sexo = "hombre"
+sexo = "mujer"
+
+#Sustituye la variable saludo de acuerdo al sexo del receptor.
+saludo = ""
+
+# TU CODIGO AQUI
+
+saludo
