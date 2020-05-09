e51c49586e312e6f7560b7e91946885d3d2e38f2
Giovani Rodriguez <travelli@live.com.mx>
2020-05-08 19:37:23 -0500

    Ejemplo 9

---
+++ ej9.py
@@ -0,0 +1,24 @@
+print("Menu Interactivo")
+
+msg = """
+Escoge una de las siguientes opciones:
+1) Sumar dos numeros.
+2) Concatenar dos cadenas.
+3) Comparar dos variables.
+4) Salir
+
+"""
+
+while(True):
+    print(msg)
+    num = int(input("Ingresa tu elección: "))
+
+    #Tu programa va aqui
+
+
+
+    #===================
+
+    if num == 4:
+        break
+
