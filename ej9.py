e9bda8588ff925bdc943dd4be3bb9ae252300afd
Giovani Rodriguez <travelli@live.com.mx>
2020-05-08 20:33:19 -0500

    Ejercicio 9 completo

--- ej9.py
+++ ej9.py
@@ -11,21 +11,38 @@ Escoge una de las siguientes opciones:
 
 while(True):
     print(msg)
-    num = int(input("Ingresa tu elección: "))
+    esEntero = True
+    try:
+        num = int(input("Ingresa tu elección: "))
+    except:
+        esEntero = False
+        print("Ingresa un valor entero valido")
 
-    #Tu programa va aqui
+    if esEntero:
+        #Tu programa va aqui
 
-    if num == 1:
-        #Haz lo del caso 1
-    elif num == 2:
-        #Haz lo del caso 2
-    elif num == 3:
-        #Haz lo del caso 3
+        if num == 1:
+            #Haz lo del caso 1
+            num1 = int(input("Ingresa el primer numero: "))
+            num2 = int(input("Ingresa el segundo numero: "))
+            print("La suma de tus dos numeros es: {}".format(num1 + num2))
+        elif num == 2:
+            #Haz lo del caso 2
+            cad1 = input("Ingresa la primera cadena: ")
+            cad2 = input("Ingresa la segunda cadena: ")
+            print("La concatenacion de las cadenas es: {}".format(cad1 + cad2))
+        elif num == 3:
+            #Haz lo del caso 3
+            var1 = input("Ingresa la primera variable: ")
+            var2 = input("Ingresa la segunda variable: ")
+            if var1 == var2:
+                print("Las variables son iguales.")
+            else:
+                print("Las variables son diferentes.")
+        #===================
 
-    #===================
-
-    elif num == 4:
-        break
-    else:
-        print("Ingresa una opción válida")
+        elif num == 4:
+            break
+        else:
+            print("Ingresa una opción válida")
 
