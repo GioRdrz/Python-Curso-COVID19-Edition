1f54b8a4319187e404466ce8278e57c206c9e075
Giovani Rodriguez <travelli@live.com.mx>
2020-05-11 19:59:24 -0500

    Ejercicios 12 y 13

---
+++ ej12.py
@@ -0,0 +1,37 @@
+"""
+Hacer un menu asemejado a un restaurante.
+Añadir 3 productos, cada producto tiene un precio (numero real).
+El cliente puedes comprar en multiples ocasiones multiples productos.
+¿Que producto quieres? 1
+¿Cuantas unidades del producto 1 quieres? 5
+
+Cuando el cliente decida la opcion de terminar de comprar, mostrar la cuenta final.
+
+Total: $0.00
+
+1) Cocota - $15
+2) Hamburguesa - $60
+3) Paponas - $35
+4) Terminar
+
+¿Que producto quieres agregar a la cuenta?
+2
+¿Cuantas hamburguesas quieres?
+3
+
+Total: $180.00
+
+
+cuenta = 0
+
+while True:
+    ....
+
+
+
+    -> ya le preguntaste al usuario que comida y cuantas unidades
+
+    cuenta += precio_cocota * cantidad
+
+"""
+
