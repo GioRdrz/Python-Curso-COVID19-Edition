3b8bd16d35fe20565964413892f5079ca0139069
Giovani Rodriguez <travelli@live.com.mx>
2020-05-18 19:16:37 -0500

    Ej 13 cuadros negros

--- ej13.py
+++ ej13.py
@@ -1,53 +1,75 @@
-# Import a library of functions called 'pygame'
+#Import a library of functions called 'pygame'
 import pygame
 
-# Initialize the game engine
+#Initialize the game engine
 pygame.init()
 
-# Define some colors
-BLACK = (0, 0, 0)
+#Define some colors
+BLACK = (0, 0, 0) 
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
-PI = 3.141592653
-
-# Set the height and width of the screen
+#Set the height and width of the screen
 size = (600, 400)
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Cuadritos del Gio")
 
-# Loop until the user clicks the close button.
+#Loop until the user clicks the close button.
 done = False
 clock = pygame.time.Clock()
 
-# Loop as long as done == False
+#Grid de cuadrados
+max_i = 54
+max_j = 36
+cuadros = [[0 for j in range(max_j)] for i in range(max_i)]
+
+#Dibujar cuadrado
+def dibujaCuadro(screen,i,j,cuadros):
+    if cuadros[i][j] == 0:
+        #Aqui si el cuadro no esta activo
+    elif cuadros[i][j] == 1:
+        #Aqui si el cuadro esta activo
+
+#Checar si mouse sobre cuadro
+def mouseSobreCuadro(x,y):
+    global cuadros
+    for i in range(len(cuadros)):
+        for j in range(len(cuadros[0])):
+            #Aqui validamos si el mouse esta posicionado dentro de algun cuadro
+            
+
+#Loop as long as done == False
 while not done:
 
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             done = True  # Flag that we are done so we exit this loop
 
-    # All drawing code happens after the for loop and but
-    # inside the main while not done loop.
+    #All drawing code happens after the for loop and but
+    #inside the main while not done loop.
 
-    # Clear the screen and set the screen background
+    #Clear the screen and set the screen background
     screen.fill(WHITE)
 
-    for i in range(54):
-        for j in range(36):
-            # Draw a rectangle
-            pygame.draw.rect(screen, BLACK, [11*i+1, 11*j+1, 10, 10], 0)
+    if pygame.mouse.get_pressed()[0]:
+        x,y = pygame.mouse.get_pos()
+        mouseSobreCuadro(x,y)
 
-    # Go ahead and update the screen with what we've drawn.
-    # This MUST happen after all the other drawing commands.
+    for i in range(max_i):
+        for j in range(max_j):
+            # Draw a rectangle
+            dibujaCuadro(screen,i,j,cuadros)
+
+    #Go ahead and update the screen with what we've drawn.
+    #This MUST happen after all the other drawing commands.
     pygame.display.flip()
 
-    # This limits the while loop to a max of 60 times per second.
-    # Leave this out and we will use all CPU we can.
-    clock.tick(60)
+    #This limits the while loop to a max of 60 times per second.
+    #Leave this out and we will use all CPU we can.
+    clock.tick(15)
 
-# Be IDLE friendly
+#Be IDLE friendly
 pygame.quit()
